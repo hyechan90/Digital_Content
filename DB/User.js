291e9a18775ca7aa4ec95f94a71b7db334b4d12c
hyechan90 <hyechan90@naver.com>
2019-11-07 08:08:03 +0900

    DB changed

--- DB/User.js
+++ DB/User.js
@@ -1,35 +1,39 @@
 // 로컬 데이터 베이스에 접속
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/userData', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userData", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 var db = mongoose.connection;
 
 // 맨 처음 연결
-db.on('error', console.error.bind(console,'Connection Error:'));
-db.once('open',function callback(){
-    console.log('DB On!');
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", function callback() {
+  console.log("DB On!");
 });
 
 // 유저 스키마
 var User = new mongoose.Schema({
-    id: String
-    ,password: String
-    ,email: String
-    ,phone: String
-    ,age: Number
-    ,itemID: String
+  id: String,
+  password: String,
+  email: String,
+  phone: String,
+  age: Number,
+  itemID: String
 });
 
 // 아이템 스키마
 var Item = new mongoose.Schema({
-    name: String
-    ,place: String
-    ,date: Date
+  name: String,
+  place: String,
+  date: Date
 });
 
 // 유저 스키마 전송
-var userModel = mongoose.model('user',User);
+var userModel = mongoose.model("user", User);
 exports.User = userModel;
 
 // 아이템 스키마 전송
-var itemModel = mongoose.model('item',Item);
+var itemModel = mongoose.model("item", Item);
 exports.Item = itemModel;
