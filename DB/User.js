ea17f9e1fcda069d7daf31eebc4bce00dbd554ef
hyechan90 <hyechan90@naver.com>
2019-11-08 20:37:46 +0900

    2019 11 08

--- DB/User.js
+++ DB/User.js
@@ -1,39 +1,37 @@
 // 로컬 데이터 베이스에 접속
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/userData", {
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/userData', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 var db = mongoose.connection;
 
 // 맨 처음 연결
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once("open", function callback() {
-  console.log("DB On!");
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', function callback() {
+  console.log('DB On!');
 });
 
 // 유저 스키마
 var User = new mongoose.Schema({
-  id: String,
+  name: String,
   password: String,
   email: String,
-  phone: String,
-  age: Number,
-  schoolID: Number
+  schoolId: String,
 });
 
 // 아이템 스키마
 var Item = new mongoose.Schema({
   name: String,
   place: String,
-  date: Date
+  date: Date,
 });
 
 // 유저 스키마 전송
-var userModel = mongoose.model("user", User);
+var userModel = mongoose.model('user', User);
 exports.User = userModel;
 
 // 아이템 스키마 전송
-var itemModel = mongoose.model("item", Item);
+var itemModel = mongoose.model('item', Item);
 exports.Item = itemModel;
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,21 @@
-var express = require("express");
-var multer = require("multer");
-var path = require("path");
-var crypto = require("crypto");
-var fs = require("fs");
+var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var crypto = require('crypto');
+var fs = require('fs');
 var app = express();
 
 // DB/User 안에 있는 데이터들을 갖어옴
-var { User, Item } = require("../DB/User");
+var { User, Item } = require('../DB/User');
 
 // GET
-app.get("/", (req, res) => {
-  console.log("Server Running...");
-  res.send("Server Running...");
+app.get('/', (req, res) => {
+  console.log('Server Running...');
+  res.send('Server Running...');
 });
 
 // 모든 유저 반환
-app.get("/get/User", (req, res) => {
+app.get('/get/User', (req, res) => {
   let time = req.query.time;
   // 1 = 오랜된 순
   // -1 = 최신순
@@ -28,7 +28,7 @@ app.get("/get/User", (req, res) => {
     });
 });
 
-app.get("/get/Item", (req, res) => {
+app.get('/get/Item', (req, res) => {
   let time = req.query.time;
   // 1 = 오랜된 순
   // -1 = 최신순
@@ -44,10 +44,10 @@ app.get("/get/Item", (req, res) => {
 // POST
 
 // 회원가입
-app.post("/post/register", (req, res) => {
+app.post('/post/register', (req, res) => {
   console.log(req.body);
   const email = req.body.email;
-  const id = req.body.id;
+  const schoolId = req.body.schoolId;
   User.findOne({ email: email }, function(err, result) {
     if (err) throw err;
     if (result == null) {
@@ -57,12 +57,11 @@ app.post("/post/register", (req, res) => {
           User.findOne({ schoolId: schoolId });
           if (result == null) {
             //새로 만들때
-            let user = new User({
-              id: req.body.id,
+            const user = new User({
+              name: req.body.name,
               password: req.body.password,
-              email: email,
-              age: req.body.age,
-              phone: req.body.phone
+              email: req.body.email,
+              schoolId: req.body.schoolId,
             });
             user.save((err, result) => {
               if (err) throw err;
@@ -71,26 +70,26 @@ app.post("/post/register", (req, res) => {
             });
           } else {
             // 이미 아이디 또는 계정이 있을 때
-            console.log("이미 사용 중인 아이디가 있습니다.");
-            res.status(404).send({ msg: "이미 사용 중인 아이디가 있습니다." });
+            console.log('이미 사용 중인 아이디가 있습니다.');
+            res.status(404).send({ msg: '이미 사용 중인 아이디가 있습니다.' });
           }
         });
       } else {
-        console.log("이메일 형식에 맞지 않습니다.");
-        res.status(404).send({ msg: "이메일 형식에 맞지 않습니다." });
+        console.log('이메일 형식에 맞지 않습니다.');
+        res.status(404).send({ msg: '이메일 형식에 맞지 않습니다.' });
       }
     } else {
-      console.log("이미 사용 중인 계정이 있습니다.");
-      res.status(404).send({ msg: "이미 사용 중인 계정이 있습니다." });
+      console.log('이미 사용 중인 계정이 있습니다.');
+      res.status(404).send({ msg: '이미 사용 중인 계정이 있습니다.' });
     }
   });
 });
 
 // 유저 로그인
-app.post("/post/login", (req, res) => {
+app.post('/post/login', (req, res) => {
   let login = new User({
     id: req.body.id,
-    passwd: req.body.passwd
+    passwd: req.body.passwd,
   });
   User.findOne({ id: login.id }, function(err, result) {
     if (err) throw err;
@@ -98,21 +97,21 @@ app.post("/post/login", (req, res) => {
       // 만약 계정이 있을 때
       if (result.passwd != login.passwd) {
         // 만약 비밀번호가 틀렸을 때
-        console.log("잘못된 비번입니다.");
-        res.status(404).send({ msg: "잘못된 비번입니다." });
+        console.log('잘못된 비번입니다.');
+        res.status(404).send({ msg: '잘못된 비번입니다.' });
       } else {
         console.log(result);
         res.send(result);
       }
     } else {
-      console.log("없는 계정입니다.");
-      res.status(404).send({ msg: "없는 계정입니다." });
+      console.log('없는 계정입니다.');
+      res.status(404).send({ msg: '없는 계정입니다.' });
     }
   });
 });
 
 // 유저 삭제
-app.post("/post/deleteUser", (req, res) => {
+app.post('/post/deleteUser', (req, res) => {
   const id = req.body.id;
   const password = req.body.password;
   User.findOne({ id: id }).exec((err, result) => {
@@ -128,18 +127,18 @@ app.post("/post/deleteUser", (req, res) => {
           res.send(result);
         });
       } else {
-        console.log("잘못된 비번입니다.");
-        res.status(404).send({ msg: "잘못된 비번입니다." });
+        console.log('잘못된 비번입니다.');
+        res.status(404).send({ msg: '잘못된 비번입니다.' });
       }
     } else {
-      console.log("없는 아이디입니다.");
-      res.status(404).send({ msg: "잘못된 비번입니다." });
+      console.log('없는 아이디입니다.');
+      res.status(404).send({ msg: '잘못된 비번입니다.' });
     }
   });
 });
 
 // 물품 등록
-app.post("/post/addItem", (req, res) => {
+app.post('/post/addItem', (req, res) => {
   let id = req.body.id;
   User.findOne({ id: id }).exec((err, result) => {
     if (err) throw err;
@@ -149,13 +148,13 @@ app.post("/post/addItem", (req, res) => {
         console.log(result);
       });
     } else {
-      console.log("잘못된 값이 전달되었습니다.");
-      res.status(500).send({ msg: "잘못된 값이 전달되었습니다." });
+      console.log('잘못된 값이 전달되었습니다.');
+      res.status(500).send({ msg: '잘못된 값이 전달되었습니다.' });
     }
   });
 });
 
-app.post("/post/changeUser", (req, res) => {
+app.post('/post/changeUser', (req, res) => {
   var id = req.body.id;
   User.findOne({ id: id }).exec((err, result) => {
     if (err) throw err;
